704b57bfadaeb84c60a13a6e03e3fa189b8da11c
Anuj04432 <anujwagmore835@gmail.com>
2026-07-06 22:07:51 +0530

    Checked get and post requests

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from models.prompt import PromptRequest ,PromptResponse
 
 app = FastAPI()
 
 @app.post("/optimize")
 def optimize():
     return {"message":"This is a prompt optimizer"}
+
+@app.get("/")
+def get(response: PromptResponse):
+    return response.optimized_prompt
+    
+
+@app.post("/check")
+def prompt(request: PromptRequest):
+    return {"prompt":request.prompt }
+        
+    
