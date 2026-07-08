66a2a1923069e8f8d85e2ebe4d6849d2d0e7e78a
Anuj04432 <anujwagmore835@gmail.com>
2026-07-08 19:32:37 +0530

    Function for detecting intents

--- app/services/intent.py
+++ app/services/intent.py
@@ -1,1 +1,11 @@
-                
+from app.rules.initent import intent
+
+def detect_intent(prompt):
+    prompt = prompt.lower()
+
+    for intents, keywords in intent.items():
+        for keyword in keywords:
+            if keyword in prompt:
+                return intents
+            
+    return {}
