841a2a0827682ed1338f30a25ea2fab090b2d26e
Anuj04432 <anujwagmore835@gmail.com>
2026-07-11 15:17:14 +0530

    Addedmore intents and fixed some issues

--- app/rules/intents.py
+++ app/rules/intents.py
@@ -1,10 +1,7 @@
 intent = {
-    "code":["python","java","c",'php','javascript','html','css''algorithm', 'sql', "bug", "debug", "function", "class", "api", "fastapi", 'stremlit'],
+    "code":["python","java",'php','javascript','html','css''algorithm', 'sql', "bug", "debug", "function", "class", "api", "fastapi", 'stremlit'],
     "explain":['explain','what', "why", 'how', 'define', 'meaning', 'concept', 'difference', 'describe'],
     "email":['email', 'mail', 'subject', 'reply', 'compose'],
     "resume":["resume","cv","curriculam vitae"],
     "essay":["essay",'article','paragraph','report','assignment'],
-    
-
-
 }
