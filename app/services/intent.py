391586d1d5de27e57af40c69a59a67f509207130
Anuj04432 <anujwagmore835@gmail.com>
2026-07-11 15:15:50 +0530

    Fixed bug in intent.py

--- app/services/intent.py
+++ app/services/intent.py
@@ -1,11 +1,19 @@
-from app.rules.initent import intent
+from app.rules.intents import intent
 
 def detect_intent(prompt):
     prompt = prompt.lower()
 
+    prompt_intent = []
+
     for intents, keywords in intent.items():
         for keyword in keywords:
             if keyword in prompt:
-                return intents
-            
-    return {}
+                prompt_intent.append(intents)
+
+    if prompt_intent:
+        return {"intents":[i for i in set(prompt_intent)]}
+    else:       
+        return "unknown"
+    
+
+print(detect_intent("describe  the python code"))
