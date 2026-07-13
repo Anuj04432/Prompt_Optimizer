1d9e12dac57bdd4381bd5ef11a3ef0788cd6fe37
Anuj04432 <anujwagmore835@gmail.com>
2026-07-13 11:46:16 +0530

    created get endpoints for checkin the response

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from models.prompt import PromptRequest ,PromptResponse
+from services.intent import detect_intent
+from services.preprocess import optimize
 
 app = FastAPI()
 
 @app.post("/optimize")
-def optimize():
+def home():
     return {"message":"This is a prompt optimizer"}
 
 @app.get("/")
@@ -15,5 +17,23 @@ def get(response: PromptResponse):
 @app.post("/check")
 def prompt(request: PromptRequest):
     return {"prompt":request.prompt }
-        
+
+@app.get("/findintent/{prompt}")
+
+def check_intent(prompt: str):
+    intent = detect_intent(prompt)
+
+    return {
+        "prompt":prompt,
+        "intent":intent
+    }
+
+@app.get("/preprocess/{prompt}")
+def get_preprocess(prompt: str):
+    pre = optimize(prompt)
+
+    return {
+        "optimized prompt":pre
+    }
+
     
