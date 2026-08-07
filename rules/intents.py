7899eda91cb2dea81eb8b095e30bd1d152c07c49
Anuj04432 <anujwagmore835@gmail.com>
2026-08-07 11:24:46 +0530

    Fixed - **Bug:** Missing comma merged 'css' and 'algorithm' into cssalgorithm

---
+++ rules/intents.py
@@ -0,0 +1,7 @@
+intent = {
+    "code":["python","java",'php','javascript','html','css','algorithm', 'sql', "bug", "debug", "function", "class", "api", "fastapi", 'stremlit'],
+    "explain":['explain','what', "why", 'how', 'define', 'meaning', 'concept', 'difference', 'describe'],
+    "email":['email', 'mail', 'subject', 'reply', 'compose'],
+    "resume":["resume","cv","curriculam vitae"],
+    "essay":["essay",'article','paragraph','report','assignment'],
+}
